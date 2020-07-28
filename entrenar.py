70e7a756d94861d1dcd4479f276990f43aebb3cf
DiegoIDSUP <153241@ids.upchiapas.edu.mx>
2020-07-28 16:16:20 -0500

    Cambio de epocas

--- entrenar.py
+++ entrenar.py
@@ -3,4 +3,4 @@ from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("entrenar")
-model_trainer.trainModel(num_objects=4, num_experiments=5, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=4, num_experiments=110, enhance_data=True, batch_size=32, show_network_summary=True)
